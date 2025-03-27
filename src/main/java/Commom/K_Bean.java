b8bc46e6f713d759b0ae6a322679d5715138d899
lovemove7272 <lovemove7272@gmail.com>
2025-03-27 10:39:15 +0900

    K_Bean

---
+++ src/main/java/Commom/K_Bean.java
@@ -0,0 +1,127 @@
+package Commom;
+
+public class K_Bean {
+	
+	private String staff_id = "";       //スタッフID
+	private String podition = "";       //役職
+	private String name = "";           //氏名
+	private String job_name = "";       //職種名
+	private String tel = "";            //電話番号
+	private String sex = "";            //性別
+	private String rank = "";           //権限
+	private String address = "";        //住所
+	private String residence = "";      //建物名
+	private String avatar = "";         //アバター
+	private String reserve_num = "";    //予約番号
+	private String reserve_day = "";    //予約日付
+	private String reserve_time = "";   //予約時間
+	private String reserve_comment = "";//予約コメント
+	private String plan_id = "";        //プランID
+	private String birth_day = "";      //生年月日
+	
+	
+	
+	
+	
+	
+	public String getStaff_id() {
+		return staff_id;
+	}
+	public void setStaff_id(String staff_id) {
+		this.staff_id = staff_id;
+	}
+	public String getPodition() {
+		return podition;
+	}
+	public void setPodition(String podition) {
+		this.podition = podition;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getJob_name() {
+		return job_name;
+	}
+	public void setJob_name(String job_name) {
+		this.job_name = job_name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getRank() {
+		return rank;
+	}
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getResidence() {
+		return residence;
+	}
+	public void setResidence(String residence) {
+		this.residence = residence;
+	}
+	public String getAvatar() {
+		return avatar;
+	}
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+	public String getReserve_num() {
+		return reserve_num;
+	}
+	public void setReserve_num(String reserve_num) {
+		this.reserve_num = reserve_num;
+	}
+	public String getReserve_day() {
+		return reserve_day;
+	}
+	public void setReserve_day(String reserve_day) {
+		this.reserve_day = reserve_day;
+	}
+	public String getReserve_time() {
+		return reserve_time;
+	}
+	public void setReserve_time(String reserve_time) {
+		this.reserve_time = reserve_time;
+	}
+	public String getReserve_comment() {
+		return reserve_comment;
+	}
+	public void setReserve_comment(String reserve_comment) {
+		this.reserve_comment = reserve_comment;
+	}
+	public String getPlan_id() {
+		return plan_id;
+	}
+	public void setPlan_id(String plan_id) {
+		this.plan_id = plan_id;
+	}
+	public String getBirth_day() {
+		return birth_day;
+	}
+	public void setBirth_day(String birth_day) {
+		this.birth_day = birth_day;
+	}
+
+	
+	
+	
+}
