3b99e697ea10be65b90a5fadc20627d4e688ca34
haruka019 <hrk019@gmail.com>
2025-04-15 16:00:52 +0900

    Plan(長田)
    Plan(長田)

---
+++ Plan(長田)/common(bean)/K_Bean.java
@@ -0,0 +1,125 @@
+package common;
+
+public class K_Bean {
+	
+	private String plan_id;			//プランID
+	private String t_plan_id;		//テーブル用プランID
+	private String plan_name;		//プラン名
+	private String stock;			//在庫
+	private String cost;			//単価
+	private String subplan;			//サブプラン
+	private String contents;		//プラン内容
+	private String time_required;	//正味所要
+	private String plan_required;	//プラン所要
+	private String periodstart;		//開始日
+	private String periodend;		//終了日
+	private String staff_memo;		//スタッフメモ
+	private String job_id1;			//職種ID1
+	private String job_id2;			//職種ID2
+	private String job_id3;			//職種ID3
+	private String image;			//画像
+	
+	
+	public String getPlan_id() {
+		return plan_id;
+	}
+	public void setPlan_id(String plan_id) {
+		this.plan_id = plan_id;
+	}
+	public String getPlan_name() {
+		return plan_name;
+	}
+	public void setPlan_name(String plan_name) {
+		this.plan_name = plan_name;
+	}
+	public String getStock() {
+		return stock;
+	}
+	public void setStock(String stock) {
+		this.stock = stock;
+	}
+	public String getCost() {
+		return cost;
+	}
+	public void setCost(String cost) {
+		this.cost = cost;
+	}
+	public String getSubplan() {
+		return subplan;
+	}
+	public void setSubplan(String subplan) {
+		this.subplan = subplan;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getTime_required() {
+		return time_required;
+	}
+	public void setTime_required(String time_required) {
+		this.time_required = time_required;
+	}
+	public String getPlan_required() {
+		return plan_required;
+	}
+	public void setPlan_required(String plan_required) {
+		this.plan_required = plan_required;
+	}
+	
+	public String getStaff_memo() {
+		return staff_memo;
+	}
+	public void setStaff_memo(String staff_memo) {
+		this.staff_memo = staff_memo;
+	}
+
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getT_plan_id() {
+		return t_plan_id;
+	}
+	public void setT_plan_id(String t_plan_id) {
+		this.t_plan_id = t_plan_id;
+	}
+	public String getPeriodstart() {
+		return periodstart;
+	}
+	public void setPeriodstart(String periodstart) {
+		this.periodstart = periodstart;
+	}
+	public String getPeriodend() {
+		return periodend;
+	}
+	public void setPeriodend(String periodend) {
+		this.periodend = periodend;
+	}
+	public String getJob_id1() {
+		return job_id1;
+	}
+	public void setJob_id1(String job_id1) {
+		this.job_id1 = job_id1;
+	}
+	public String getJob_id2() {
+		return job_id2;
+	}
+	public void setJob_id2(String job_id2) {
+		this.job_id2 = job_id2;
+	}
+	public String getJob_id3() {
+		return job_id3;
+	}
+	public void setJob_id3(String job_id3) {
+		this.job_id3 = job_id3;
+	}
+	
+}
+	
+	
+	
