6a2df1e63c9938e4886aa0f806e5f386219ce226
岡谷 <lovemove7272@gmail.com>
2025-04-16 16:05:11 +0900

    Add files via upload

---
+++ karakuriインサート文/reserve.sql
@@ -0,0 +1,7 @@
+insert into academy.reserve(reserve_day,reserve_time,reserve_comment,reserve_cost,reserve_name,reserve_kana,reserve_tel,reserve_email,reserve_nationality,reserve_media,plan_id,createdby,createddate,updatedby,updateddate) values 
+    (DATE '2025-04-23',TIME '16:00:00.000','これはテストデータです',1000,'テスト ユーザー',null,'999999999',null,null,null,1004,'testuser',DATE '2025-04-15','testuser',DATE '2025-04-15')
+  , (DATE '2025-04-20',TIME '15:00:00.000','着物が楽しみ',5000,'渡辺 良子',null,'222333444',null,null,null,1001,'testuser',DATE '2025-04-15','testuser',DATE '2025-04-15')
+  , (DATE '2025-04-21',TIME '14:30:00.000','記念撮影希望',8000,'佐藤 太郎',null,'444555666',null,null,null,1002,'testuser',DATE '2025-04-15','testuser',DATE '2025-04-15')
+  , (DATE '2025-04-20',TIME '11:00:00.000','初めての花魁体験',12000,'鈴木 次郎',null,'777888999',null,null,null,1003,'testuser',DATE '2025-04-15','testuser',DATE '2025-04-15')
+  , (DATE '2025-04-20',TIME '10:00:00.000','桜が見たいです',5000,'山田 花子',null,'111222333',null,null,null,1001,'testuser',DATE '2025-04-15','testuser',DATE '2025-04-15')
+  , (DATE '2025-04-22',TIME '09:00:00.000',null,5000,'高橋 直樹',null,'123123123',null,null,null,1001,'testuser',DATE '2025-04-15','testuser',DATE '2025-04-15');
---
+++ karakuriインサート文/staff.sql
@@ -0,0 +1,11 @@
+insert into academy.staff(job_id,birth_day,staff_id,name,sex,podition,address,residence,tel,avatar,rank_id,createdby,createddate,updatedby,updateddate) values 
+    (1,DATE '1993-10-19',1001,'長田悠',1,2,'夢の国','シンデレラパレス','123456789','image/nagata.png',0,null,null,null,null)
+  , (3,DATE '2001-10-12',1002,'馬場俊介',2,0,'バチカン市国','バチカン美術館','123456789','image/baba.png',0,null,null,null,null)
+  , (5,DATE '1999-12-18',1003,'岡田奈々',1,0,'北海道','旭川動物園','123456789','image/okada.png',1,null,null,null,null)
+  , (2,DATE '1986-05-26',1004,'戸高永',1,0,'東京','東京タワー','123456789','image/Todaka.png',0,null,null,null,null)
+  , (4,DATE '1993-10-19',1006,'はーちゃん',1,2,'銀河系','火星','123456789','image/nagata_D.png',0,null,null,null,null)
+  , (6,DATE '2001-10-12',1007,'ばばきゅん',2,0,'沖縄','美ら海水族館','123456789','image/baba_D.png',1,null,null,null,null)
+  , (4,DATE '1999-12-18',1008,'なーちゃん',1,1,'沖縄','国際通り','123456789','image/okada_D.png',1,null,null,null,null)
+  , (5,DATE '1986-05-26',1009,'きしもと',1,1,'ハワイ','ワイキキビーチ','123456789','image/todaka_D.png',1,null,null,null,null)
+  , (2,DATE '1985-07-22',1005,'岡谷奈津規',1,0,'フランス','ベルサイユ宮殿','123456789','image/okatani.png',0,null,null,null,null)
+  , (6,DATE '1985-07-22',1010,'おかやん',1,1,'長崎','軍艦島','123456789','image/okatani_D.png',1,null,null,null,null);
